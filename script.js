91643723a412695f2419747baa09af81b698d1d7
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-04-16 19:19:03 +0300

    Added eventListener

---
+++ script.js
@@ -0,0 +1,14 @@
+const startBtn = document.querySelector("#start"),
+    screens = document.querySelectorAll(".screen"),
+    timeList = document.querySelector("#time-list"),
+    difficultyList = document.querySelector("#difficulty-list");
+let time = 0,
+    unlimited = false,
+    difficulty = 0;
+
+
+startBtn.addEventListener("click",()=>{
+    screen[0].classList.add("up");
+});
+
+
